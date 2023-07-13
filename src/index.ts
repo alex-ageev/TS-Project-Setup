9220bfba5abc17bc316e42e0a6e6c6b6b682861f
Alexander Ageev <alex.ageev.miew@gmail.com>
2023-07-13 12:22:06 +0100

    no message

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { testFunction } from './modules/test.js'
+
+const msg: string = 'Hello NodeJS with TS!';
+console.log(msg)
+
+testFunction('index.ts')
---
+++ src/modules/test.ts
@@ -0,0 +1,3 @@
+export function testFunction(name: string): void {
+  console.log(name, 'works...')
+}
